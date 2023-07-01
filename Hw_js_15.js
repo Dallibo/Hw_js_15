a72d47810c6dbed9acad4e564ef8b3a56d047882
Dallibo <denismalyarenko1@gmail.com>
2023-07-01 12:18:29 +0300

    2 tasks out of 3

--- Hw_js_15.js
+++ Hw_js_15.js
@@ -12,27 +12,34 @@ class PrintMachine {
   const printer = new PrintMachine(16, 'blue', 'Tahoma, Arial, sans-serif;');
   printer.print('Hellow Web-31');
 
-  class News {
-    constructor(Title, Text, Tags, Date) {
-      this.Title = Title;
-      this.Text = Text;
-      this.Tags = Tags;
-      this.date = Date;
-    }
-    print() {
-      let day1 = new Date(2023, 5, 28, 4, 26, 45, 456);
-      let diff = new Date() - day1;
-      let days = diff / (1000 * 60 * 60 * 24);
-  
-      if (days < 1) {
-        console.log("today");
-      } else if (days < 7) {
-        console.log(`${parseInt(days)} days ago`);
-      } else {
-        console.log(`${day1.getDate()}.${day1.getMonth() + 1}.${day1.getFullYear()}`);
-      }
-      document.write(`<p> ${Title}</p>`)
-    }
+class News {
+  constructor(title, text, tags, date) {
+    this.title = title;
+    this.text = text;
+    this.tags = tags;
+    this.date = date;
   }
 
-  let news1 = new News("header 1", "lorem impus dolor",["sport", "summer", "tennis"] )
+  print() {
+  let day1 = new Date(2023, 5, 29, 4, 26, 45, 456);
+  let diff = new Date() - day1;
+  let days = diff / (1000 * 60 * 60 * 24);
+  let dateOutput;
+
+  if (days < 1) {
+    dateOutput = "today";
+  } else if (days < 7) {
+    dateOutput = `${parseInt(days)} days ago`;
+  } else {
+    dateOutput = `${day1.getDate()}.${day1.getMonth() + 1}.${day1.getFullYear()}`;
+  }
+
+  let str = `<h2>${this.title}</h2>`;
+  str += `<p>${dateOutput}</p>`;
+  str += `<p>${this.text}</p>`;
+  str += "<div>" + Array.from(this.tags).map(elem => `#${elem}`).join(" ") + "</div>";
+  document.write(str);
+}
+}
+let news1 = new News("Hellow Web-31", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis doloremque, ducimus ab ut et obcaecati reprehenderit nisi eaque officia animi excepturi porro harum dolorem voluptates corrupti fugit dolores ipsum? Ipsam!", ["sport", "summer", "tennis"], new Date());
+[news1].forEach(elem => elem.print());
