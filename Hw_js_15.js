8203568c16d13000010a4093cd469502380f6e05
Dallibo <denismalyarenko1@gmail.com>
2023-07-01 12:27:52 +0300

    all HW

--- Hw_js_15.js
+++ Hw_js_15.js
@@ -43,3 +43,61 @@ class News {
 }
 let news1 = new News("Hellow Web-31", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis doloremque, ducimus ab ut et obcaecati reprehenderit nisi eaque officia animi excepturi porro harum dolorem voluptates corrupti fugit dolores ipsum? Ipsam!", ["sport", "summer", "tennis"], new Date());
 [news1].forEach(elem => elem.print());
+
+
+class NewsString {
+  constructor() {
+    this.newsArray = [];
+  }
+    get count() {
+    return this.newsArray.length;
+  }
+  displayAllNews() {
+    this.newsArray.forEach(news => console.log(news));
+  }
+  addNews(news) {
+    this.newsArray.push(news);
+  }
+  removeNews(news) {
+    const index = this.newsArray.indexOf(news);
+    if (index !== -1) {
+      this.newsArray.splice(index, 1);
+    }
+  }
+  sortNewsByDate() {
+    this.newsArray.sort((a, b) => b.date - a.date);
+  }
+
+  searchNewsByTag(tag) {
+    return this.newsArray.filter(news => news.tags.includes(tag));
+  }
+}
+let newsString = new NewsString();
+newsString.addNews({
+  title: "News 1",
+  text: "Новость 1",
+  tags: ["tag1", "tag2"],
+  date: new Date(2023, 5, 28, 4, 26, 45, 456)
+});
+
+newsString.addNews({
+  title: "News 2",
+  text: "Новость 2",
+  tags: ["tag2", "tag3"],
+  date: new Date(2023, 5, 29, 12, 30, 0, 0)
+});
+
+console.log("Number of news:", newsString.count);
+newsString.displayAllNews();
+
+newsString.sortNewsByDate();
+console.log("Sorted news:");
+newsString.displayAllNews();
+
+const searchResults = newsString.searchNewsByTag("tag2");
+console.log("News with tag 'tag2':");
+searchResults.forEach(news => console.log(news));
+
+newsString.removeNews(searchResults[0]);
+console.log("After removing a news:");
+newsString.displayAllNews();
